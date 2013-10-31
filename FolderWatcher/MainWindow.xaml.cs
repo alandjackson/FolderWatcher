37111b7cbb198480a6e615d7dcfc6f7682b0503d
Alan Jackson <alandjackson@gmail.com>
2013-10-31 06:51:47 -0500

    Finished implementing form

--- FolderWatcher/MainWindow.xaml.cs
+++ FolderWatcher/MainWindow.xaml.cs
@@ -20,24 +20,68 @@ namespace FolderWatcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        FolderWatcher.Common.FolderWatcher FolderWatcherInstance = new Common.FolderWatcher();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            FolderWatcherInstance.NewFile += (s, e) => AppendStatus("New File: " + e.Param);
+        }
+
+        private void AppendStatus(string message)
+        {
+            StatusMessagesTextBlock.Text += message + Environment.NewLine;
+            StatusMessageScrollViewer.ScrollToEnd();
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            FolderTextBox.Text = FolderWatcher.Properties.Settings.Default.Folder;
         }
 
         private void BrowseFolder_Click(object sender, RoutedEventArgs e)
         {
+            var dlg = new System.Windows.Forms.FolderBrowserDialog();
 
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FolderTextBox.Text = dlg.SelectedPath;
+            }
         }
 
         private void FolderTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            FolderWatcher.Properties.Settings.Default.Folder = FolderTextBox.Text;
+            FolderWatcher.Properties.Settings.Default.Save();
         }
 
         private void HotfolderControlButton_Click(object sender, RoutedEventArgs e)
         {
+            if ((string)HotfolderStateLabel.Content == "Stopped")
+            {
+                FolderWatcherInstance.Join();
 
+                FolderWatcherInstance.FolderToWatch = FolderTextBox.Text;
+                FolderWatcherInstance.IgnoreStartingFiles = true;
+                FolderWatcherInstance.RunAsync();
+
+                AppendStatus("Started watching: " + FolderWatcherInstance.FolderToWatch);
+                HotfolderStateLabel.Content = "Running...";
+                HotfolderControlButton.Content = "Stop";
+                //StartProcessor();
+            }
+            else
+            {
+                FolderWatcherInstance.End();
+                AppendStatus("Stopped watching folder");
+                HotfolderStateLabel.Content = "Stopped";
+                HotfolderControlButton.Content = "Start";
+                //StopProcessor();
+            }
+
+            //HotfolderStateLabel.Content = RimageProcessor.IsRunning ? "Running" : "Stopped";
+            //HotfolderControlButton.Content = RimageProcessor.IsRunning ? "Stop" : "Start";
         }
 
         
